5e9cb70a046e0592872f96a04ee6d58ec0683e20
Idan <eidan47@gmail.com>
2021-03-05 00:06:53 +0200

    [refactor-2] fix input html tag.

--- src/components/Dashboard/Homepage/Homepage.jsx
+++ src/components/Dashboard/Homepage/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Homepage = () => {
-  return <div>Homepage</div>;
+const Dashboard = () => {
+  return <div>Dashboard</div>;
 };
 
-export default Homepage;
+export default Dashboard;
