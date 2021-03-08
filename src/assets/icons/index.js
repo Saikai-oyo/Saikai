724b9a97f920422d3dab6ce1b4420fb33e26935c
Idan <eidan47@gmail.com>
2021-03-08 14:32:21 +0200

    [feat-5] add export index file

---
+++ src/assets/icons/index.js
@@ -0,0 +1,4 @@
+export { default as userIcon } from './user.svg';
+export { default as settingIcon } from './setting.svg';
+export { default as logoutIcon } from './logout.svg';
+export { default as addIcon } from './add.svg';
