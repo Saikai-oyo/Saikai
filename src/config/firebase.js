e8d0a6703402b5a64ce210bc78d7897722997a51
Idan <eidan47@gmail.com>
2021-03-09 03:39:27 +0200

    [feat-5] add new export

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const app = firebase.initializeApp({
+export const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -13,5 +13,4 @@ const app = firebase.initializeApp({
 });
 
 export const auth = app.auth();
-
-export default app;
+export default firebase;
