64926b1e1aab99cb6b20821db0b9c74f234183b5
Idan <eidan47@gmail.com>
2021-03-11 23:13:38 +0200

    [refactor-3] remove all import-export index files

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Homepage } from './Dashboard/Homepage';
-export { default as Login } from './Auth/Login';
-export { default as Signup } from './Auth/Signup';
-export { default as Profile } from './User/Profile';
-export { default as ForgotPassword } from './Auth/ForgotPassword';
-export { default as UpdateProfile } from './User/UpdateProfile';
