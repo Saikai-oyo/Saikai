218c99eb7fc1a3312587802d83bcd404257de61f
Idan <eidan47@gmail.com>
2021-03-12 13:09:50 +0200

    Updates

---
+++ precache-manifest.7bc23706fe5b24f7bee2e0ea3c212870.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b765e98b59a5da41d2675a4473014e",
+    "url": "/eidan66/Saikai/index.html"
+  },
+  {
+    "revision": "a6b44c63fc7c25470f5c",
+    "url": "/eidan66/Saikai/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "86371b8b41f902b8c195",
+    "url": "/eidan66/Saikai/static/css/main.262e367a.chunk.css"
+  },
+  {
+    "revision": "a6b44c63fc7c25470f5c",
+    "url": "/eidan66/Saikai/static/js/2.a23d5e95.chunk.js"
+  },
+  {
+    "revision": "7c8c498c154c5c7fc14cf0ad148613b4",
+    "url": "/eidan66/Saikai/static/js/2.a23d5e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86371b8b41f902b8c195",
+    "url": "/eidan66/Saikai/static/js/main.3951b161.chunk.js"
+  },
+  {
+    "revision": "94f7e2af45d5b533ef86",
+    "url": "/eidan66/Saikai/static/js/runtime-main.86adde38.js"
+  },
+  {
+    "revision": "1d239fa3155a1ddd4eff5bcb53903aa8",
+    "url": "/eidan66/Saikai/static/media/add.1d239fa3.svg"
+  },
+  {
+    "revision": "d9d57df5827d149ed58a96106f7b8059",
+    "url": "/eidan66/Saikai/static/media/logo.d9d57df5.png"
+  },
+  {
+    "revision": "1baa3ff33edc56ea0cb74369bc430bea",
+    "url": "/eidan66/Saikai/static/media/logout.1baa3ff3.svg"
+  },
+  {
+    "revision": "ce34cb6a4101986d5e7bd7fc12fac5ea",
+    "url": "/eidan66/Saikai/static/media/setting.ce34cb6a.svg"
+  },
+  {
+    "revision": "cf40332eccd225cde33a2f78eb6bc99d",
+    "url": "/eidan66/Saikai/static/media/user.cf40332e.svg"
+  }
+]);
