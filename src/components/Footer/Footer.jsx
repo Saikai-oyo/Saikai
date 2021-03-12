cda1e7f5aef9740cca50a077fc030a703a6bd3f9
Idan <eidan47@gmail.com>
2021-03-12 12:34:13 +0200

    [refactor-3] restyle copyright

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,14 +6,23 @@ const Footer = () => {
       <div className='container'>
         <span className='text-muted'>
           <div className='text-center text-dark p-3 copyright'>
-            © 2021 Copyright:
-            <a
-              className='text-dark'
-              href='https://www.linkedin.com/in/idanlevian/'
-            >
-              {' '}
-              Saikai - Idan Levian
-            </a>
+            <small>
+              Copyright: © 2021{' '}
+              <a
+                className='text-dark'
+                href='https://github.com/eidan66/Saikai/'
+              >
+                {' '}
+                Saikai.
+              </a>
+              <a
+                className='text-dark'
+                href='https://www.linkedin.com/in/idanlevian/'
+              >
+                {' '}
+                Idan Levian.
+              </a>
+            </small>
           </div>
         </span>
       </div>
