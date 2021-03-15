a301dde32fd89b1f9755a657868790155edc7e15
Idan <eidan47@gmail.com>
2021-03-15 17:49:15 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Saikai/precache-manifest.848cfd7c1f542a765bde8306c8274064.js"
+  "/Saikai/precache-manifest.b4d4d7e7ad54ac1fbc12b79b274d6c98.js"
 );
 
 self.addEventListener('message', (event) => {
