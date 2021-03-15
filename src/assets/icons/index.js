aa867e27191cf23025acd0a2ead9a84528ba790a
Idan <eidan47@gmail.com>
2021-03-15 17:16:00 +0200

    [feat-8] new position to button and change colors

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,3 +2,4 @@ export { default as userIcon } from './user.svg';
 export { default as settingIcon } from './setting.svg';
 export { default as logoutIcon } from './logout.svg';
 export { default as addIcon } from './add.svg';
+export { default as addPositionSVG } from './addPosition.svg';
--- src/components/Dashboard/List/List.jsx
+++ src/components/Dashboard/List/List.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { addIcon } from '../../../assets/icons';
+import { addIcon, addPositionSVG } from '../../../assets/icons';
 import './style.css';
 
 const List = ({
@@ -17,65 +17,78 @@ const List = ({
           <div className='spinner-border text-success' role='status'></div>
         </div>
       ) : (
-        <div className='container'>
-          <div className='row mt-5 text-center'>
-            {dataList.map((data) => (
-              <div key={data.title} className='p-2 mb-4 col-md-2 cardLists'>
-                <h3 className='listTitle'>{data.title}</h3>
-                <div id='positionWrapper'>
-                  {data.items.map((company) => {
-                    const cardColorsId =
-                      company.status === 'Denied'
-                        ? 'denied'
-                        : company.status === 'Contract'
-                        ? 'contract'
-                        : company.status === 'In Progress'
-                        ? 'inProgress'
-                        : company.status === 'Receive Task'
-                        ? 'receiveTask'
-                        : company.status === 'Applied'
-                        ? 'applied'
-                        : '';
-                    return (
-                      currentUser.uid === company.uid &&
-                      data.title === company.status && (
-                        <div
-                          key={company.id}
-                          className='card cardStyle mb-2 mt-3'
-                          data-toggle='modal'
-                          data-backdrop='static'
-                          data-keyboard='false'
-                          data-target='.bd-selected-position'
-                        >
+        <div>
+          <div class='col-md-1 addButtonWrapper  mt-2 mb-2'>
+            <button
+              type='button'
+              className='addPositionButton'
+              data-toggle='modal'
+              data-target='.bd-add-position'
+              onClick={() => setAddPosition(true)}
+            >
+              <img src={addPositionSVG} className='mr-3' alt='add button' />
+            </button>
+          </div>
+          <div className='container'>
+            <div className='row row-col-6 text-center '>
+              {dataList.map((data) => (
+                <div key={data.title} className='p-2 mb-4 col-md cardLists'>
+                  <h3 className='listTitle'>{data.title}</h3>
+                  <div id='positionWrapper'>
+                    {data.items.map((company) => {
+                      const cardColorsId =
+                        company.status === 'Denied'
+                          ? 'denied'
+                          : company.status === 'Contract'
+                          ? 'contract'
+                          : company.status === 'In Progress'
+                          ? 'inProgress'
+                          : company.status === 'Receive Task'
+                          ? 'receiveTask'
+                          : company.status === 'Applied'
+                          ? 'applied'
+                          : '';
+                      return (
+                        currentUser.uid === company.uid &&
+                        data.title === company.status && (
                           <div
-                            className={`p-1 cardButton`}
-                            id={`${cardColorsId}`}
-                            onClick={() => {
-                              setError('');
-                              setMessage('');
-                              setSelectedPosition(company);
-                            }}
+                            key={company.id}
+                            className='card cardStyle mb-2 mt-3'
+                            data-toggle='modal'
+                            data-backdrop='static'
+                            data-keyboard='false'
+                            data-target='.bd-selected-position'
                           >
-                            {company.position}
-                            <br />
-                            <span className='text-white-50 font-smaller'>
-                              {company.name}
-                            </span>
+                            <div
+                              className={`p-1 cardButton`}
+                              id={`${cardColorsId}`}
+                              onClick={() => {
+                                setError('');
+                                setMessage('');
+                                setSelectedPosition(company);
+                              }}
+                            >
+                              {company.position}
+                              <br />
+                              <span className='text-white-50 font-smaller'>
+                                {company.name}
+                              </span>
+                            </div>
                           </div>
-                        </div>
-                      )
-                    );
-                  })}
+                        )
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       )}
-      <div className='d-flex justify-content-end mr-2'>
+      {/* <div className='d-flex justify-content-end mr-2'>
         <button
           type='button'
-          className='mt-3 addCompanyButton'
+          className='mt-3 addPositionButton'
           data-toggle='modal'
           data-target='.bd-add-position'
           onClick={() => setAddPosition(true)}
@@ -83,7 +96,7 @@ const List = ({
           <img src={addIcon} className='mr-3 addIcon' alt='add button' />
           Add new Position
         </button>
-      </div>
+      </div> */}
     </>
   );
 };
