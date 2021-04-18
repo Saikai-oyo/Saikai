edba23990b1b4b09abcf8f27bb12267187dc6eeb
Idan <eidan47@gmail.com>
2021-04-19 00:46:07 +0300

    [feat-10] add & remove icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,6 @@
-export { default as userIcon } from './user.svg';
 export { default as settingIcon } from './setting.svg';
 export { default as logoutIcon } from './logout.svg';
 export { default as addIcon } from './add.svg';
 export { default as addIconBlack } from './add-black.svg';
 export { default as addPositionSVG } from './addPosition.svg';
+export { default as searchIcon } from './searchIcon.svg';
