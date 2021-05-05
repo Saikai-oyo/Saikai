89db60ca6c55b19eec1a4758463f2a6ed8d100ed
Idan <eidan47@gmail.com>
2021-05-05 17:35:54 +0300

    [feat-10] remove unused lines

--- src/components/Dashboard/Homepage/Homepage.jsx
+++ src/components/Dashboard/Homepage/Homepage.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import csc from 'country-state-city';
 import { organizedData } from '../../../helpers';
 import { useAuth } from '../../../contexts/AuthContext';
 import { app } from '../../../config/firebase';
@@ -16,17 +15,9 @@ const Homepage = () => {
   const positionContext = useContext(PositionsContext);
   const { information } = useContext(MessagesContext);
 
-  const [selectedPosition, setSelectedPosition] = React.useState(null);
-  const [addPosition, setAddPosition] = React.useState(null);
-  const [userDetails, setUserDetails] = React.useState('');
-  const [cities, setCities] = React.useState([]);
   const [error, setError] = React.useState('');
-  const [addFormError, setAddFormError] = React.useState('');
   const { currentUser } = useAuth();
-  // const [dataList, setDataList] = React.useState(null);
   const [message, setMessage] = React.useState('');
-
-  const [positionForm, setPositionForm] = React.useState(null);
 
   React.useEffect(() => {
     app
@@ -46,56 +37,18 @@ const Homepage = () => {
       });
   }, []);
 
-  React.useEffect(() => {
-    var docRef = app.firestore().collection('users').doc(`${currentUser.uid}`);
-    docRef.get().then((doc) => {
-      if (doc.exists) {
-        setUserDetails(doc.data());
-      } else {
-        console.error('No doc');
-      }
-    });
-  }, []);
-
-  React.useEffect(() => {
-    try {
-      const citiesData = csc.getCitiesOfCountry('IL');
-      setCities(citiesData);
-    } catch (error) {
-      throw new Error('error:', error.message);
-    }
-  }, []);
-
   return (
     <S.HomepageWrapper>
       <Navbar
         error={information.error}
         message={information.message}
-        userDetails={userDetails}
         setError={setError}
         setMessage={setMessage}
       />
 
-      <List
-        dataList={positionContext.positions.data}
-        setError={setError}
-        setMessage={setMessage}
-        setAddPosition={setAddPosition}
-      />
+      <List />
 
-      <ShowPosition
-        selectedPosition={selectedPosition}
-        setPositionForm={setPositionForm}
-        positionForm={positionForm}
-        setSelectedPosition={setSelectedPosition}
-        dataList={positionContext.positions.data}
-        cities={cities}
-        addFormError={addFormError}
-        setAddFormError={setAddFormError}
-        setError={setError}
-        setMessage={setMessage}
-        setAddPosition={setAddPosition}
-      />
+      <ShowPosition />
     </S.HomepageWrapper>
   );
 };
