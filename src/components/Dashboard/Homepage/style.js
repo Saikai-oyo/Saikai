76a2520c25e04a889b76692ad823d2757260b172
Idan Levian <eidan47@gmail.com>
2023-06-04 20:59:55 +0300

    SIK-3: Move deprecated code to __DEPRECATED__ folder

--- src/components/Dashboard/Homepage/style.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-// import { backgroundVector } from '../../../assets/icons';
-
-export const HomepageWrapper = styled.div`
-    height: 100vh;
-`;
--- src/components/Input/index.js
+++
@@ -1,7 +0,0 @@
-export { default as AdvanceInputs } from './AdvanceInputs';
-export { default as AuthInput } from './AuthInput';
-export { default as DescriptionInput } from './DescriptionInput';
-export { default as LinkInput } from './LinkInput';
-export { default as PositionsInput } from './PositionsInput';
-export { default as DateInput } from './DateInput';
-export { default as StatusInput } from './StatusInput';
