4b1b0dc4aed1a792dd9a7eb6d410c9177205d410
Idan Levian <eidan47@gmail.com>
2023-06-18 14:39:32 +0300

    Update tests flows

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  verbose: true,
+  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/__DEPRECATED__/'],
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.setup.js',
+  ],
+};
+
+export default config;
