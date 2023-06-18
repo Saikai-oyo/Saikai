213fac2f75037b17c00e3c2ed1ab82c41a596968
Idan Levian <eidan47@gmail.com>
2023-06-18 20:11:17 +0300

    SIK-14: Add Typography component

---
+++ src/constants/colors.ts
@@ -0,0 +1,4 @@
+export const COLORS: Record<string, string> = {
+  black: '#000000',
+  white: '#FFFFFF',
+};
---
+++ src/shared/Typography/types/index.ts
@@ -0,0 +1,7 @@
+export enum TextSize {
+  XS = 'h5',
+  S = 'h4',
+  M = 'h3',
+  L = 'h2',
+  XL = 'h1',
+}
