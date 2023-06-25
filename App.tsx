3799ac387fd97eb5008a30c3018d088e89f80a20
Idan Levian <eidan47@gmail.com>
2023-06-25 13:33:44 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
+// eslint-disable-next-line import/no-named-as-default
 import Constants from 'expo-constants';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import { COLORS } from './src/constants/colors';
 import { Input } from './src/shared/Input/Input';
@@ -12,7 +13,6 @@ console.log(
   '%c Welcome to Saikai!',
   'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)',
 );
-
 
 const styles = StyleSheet.create({
   container: {
