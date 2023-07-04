7c299f95d65346d4c33e5ae2c62a1b18b681f22c
Idan Levian <eidan47@gmail.com>
2023-07-04 14:23:27 +0300

    SFE-32: Remove Deprecated folder

--- __DEPRECATED__/src/assets/images/index.js
+++
@@ -1,3 +0,0 @@
-export { default as backgroundVector } from './background-vector.png';
-export { default as addPositionBG } from './addPositionBG.svg';
-export { default as ViewModalBG } from './ViewModalBG.svg';
